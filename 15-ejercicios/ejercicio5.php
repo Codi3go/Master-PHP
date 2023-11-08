64c10248335eba4ffa3d856b9cad68d42f699470
Codi3go <110266205+Codi3go@users.noreply.github.com>
2023-11-07 22:31:20 -0500

    Ejercicio 5

--- 15-ejercicios/ejercicio5.php
+++ 15-ejercicios/ejercicio5.php
@@ -9,3 +9,28 @@
  Cada columna debe ir en un fichero separado (includes).
  
  */
+$tabla=array(
+    "ACCION"  =>array("GTA", "COD", "HALO"),
+    "AVENTURA"=>array("SUPER MARIO", "GOW", "ALADIN"),
+    "DEPORTES"=>array("BASEBALL", "FIFA", "MOTO GP")
+);
+
+
+
+$categorias= array_keys($tabla);
+?>
+<!<!doctype html>
+<html lang=""es">
+    <head>
+        <meta charset="utf-8">
+        <title>Hola</title>
+                </head>
+                <body>
+<table border="5">
+    <?php require_once 'ejercicio5/encabezado.php';?>
+    <?php require_once 'ejercicio5/primera.php';?>
+    <?php require_once 'ejercicio5/segunda.php';?>
+    <?php require_once 'ejercicio5/tercera.php';?>
+</table>
+                </body>
+</html>
---
+++ 15-ejercicios/ejercicio5/encabezado.php
@@ -0,0 +1,10 @@
+<tr>
+        <?php        foreach ($categorias as $categoria):?>
+        <th><?=$categoria?></th>
+        <?php        endforeach; ?>
+     <!--Los : dos puntos indican donde empieza el bloque de intruciones
+     de forecah, ya para finalizar dicho bloque se usa endforecah
+     este metodo de escritura se utiliza cuando se esta mezclando php
+     y html debido a que se complica usar llaves por tanto codigo revuelto-->     
+    </tr>
+
